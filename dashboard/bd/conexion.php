b590670f950cef309407e6ef51ae89d71ceb6eff
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-02-04 19:05:27 -0600

    Update conexion.php

--- dashboard/bd/conexion.php
+++ dashboard/bd/conexion.php
@@ -1,10 +1,10 @@
 <?php 
 class Conexion{	  
     public static function Conectar() {        
-        define('servidor','mariadb.csn.svc.cluster.local');
+        define('servidor','mysqldb.juan-garcia-dev.svc.cluster.local');
         define('nombre_bd','sampledb');
-        define('usuario','userRCU');
-        define('password','iDhkh1l3dpbn72VN');					        
+        define('usuario','userVPC');
+        define('password','lyEH1kUHyFWySals');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
